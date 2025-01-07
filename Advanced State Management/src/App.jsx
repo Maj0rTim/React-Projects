0d19e7c90ece176913e66c729d23f1d3ab9535cd
Timothy Fischer <timfisch242@gmail.com>
2025-01-07 12:26:51 +0200

    learning state management

---
+++ Advanced State Management/src/App.jsx
@@ -0,0 +1,51 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+  const [total, setTotal] = useState(0)
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    const updatedLeft = left + 1
+    setLeft(updatedLeft)
+    setTotal(updatedLeft + right) 
+  }
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'));
+    const updatedRight = right + 1;
+    setRight(updatedRight);
+    setTotal(left + updatedRight);
+  };
+
+  return (
+    <div>
+      {left}
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightClick}>right</button>
+      {right}
+      <p>{allClicks.join(' ')}</p>
+      <p>total {total}</p>
+      
+    </div>
+  )
+}
+
+const History = (properties) => {
+  if (properties.allClicks.length === 0) {
+    return (
+      <div>
+      App is used by pressing the button
+      </div>
+    )
+  }
+  return (
+    <div>
+      button press history: {props.allClicks.join(' ')}
+    </div>
+  )
+} 
+
+export default App
